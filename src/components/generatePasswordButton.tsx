e106829921fed9ab1afc311024118f71760c0ed5
hebertcisco <mrrabbitomg@gmail.com>
2020-10-03 18:50:52 -0300

    Add shadow on button and up the borderRadius to 50

--- src/components/generatePasswordButton.tsx
+++ src/components/generatePasswordButton.tsx
@@ -3,32 +3,41 @@ import { Button } from "react-native-paper";
 import theme from "../constants/theme";
 
 interface Props {
-  loading: boolean;
-  generatePassword: () => void;
+    loading: boolean;
+    generatePassword: () => void;
 }
 
 const GeneratePasswordButton: React.FC<Props> = ({
-  loading,
-  generatePassword,
+    loading,
+    generatePassword,
 }) => {
-  return (
-    <Button
-      style={{
-        height: 50,
-        margin: 20,
-        justifyContent: "center",
-        borderRadius: 20,
-      }}
-      labelStyle={{ fontSize: 16 }}
-      mode="contained"
-      disabled={loading}
-      loading={loading}
-      color={theme.color.purple}
-      onPress={generatePassword}
-    >
-      Generate password
-    </Button>
-  );
+    return (
+        <Button
+            style={{
+                height: 50,
+                margin: 20,
+                justifyContent: "center",
+                borderRadius: 50,
+                shadowColor: "#000",
+                shadowOffset: {
+                    width: 0,
+                    height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+
+                elevation: 5,
+            }}
+            labelStyle={{ fontSize: 16 }}
+            mode="contained"
+            disabled={loading}
+            loading={loading}
+            color={theme.color.purple}
+            onPress={generatePassword}
+        >
+            Generate password
+        </Button>
+    );
 };
 
 export default GeneratePasswordButton;
